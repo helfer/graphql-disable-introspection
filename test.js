01f1a4e6f900f6ff4fed7bce28bb9d3218129e24
Jonas Helfer <jonas@helfer.email>
2017-04-09 20:08:28 -0700

    Add tests for graphql-server-express

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 var graphql = require('graphql');
 var noIntrospection = require('./index');
 var expect = require('chai').expect;
+var express = require('express');
+var request = require('supertest-as-promised');
+var graphqlExpress = require('graphql-server-express').graphqlExpress;
+var bodyParser = require('body-parser');
 
 var schema = new graphql.GraphQLSchema({
   query: new graphql.GraphQLObjectType({
@@ -34,6 +38,51 @@ describe('NoIntrospection validation rule', function(){
 		var query = graphql.parse('{ hello }');
 		var validationErrors = graphql.validate(schema, query, [noIntrospection]);
 		return expect(validationErrors.length).to.equal(0);
+  });
+
+	describe('works with graphql-server-express', function() {
+		var app = express();
+		app.use('/graphql',
+			bodyParser.json(),
+			graphqlExpress({ schema: schema, validationRules: [noIntrospection] })
+		);
+		
+		it('disables introspection using __schema', function() {
+      var req = request(app)
+        .post('/graphql')
+        .send({
+          query: '{ __schema { queryType { name } } }'
+        });
+
+      return req.then( function(result) {
+				return expect(result.body.errors[0].message).to.match(/introspection is not allowed/);
+      });
+    });
+
+		it('disables introspection using __type', function() {
+      var req = request(app)
+        .post('/graphql')
+        .send({
+          query: '{ __type(name: "Query") { name } }'
+        });
+
+      return req.then( function(result) {
+				return expect(result.body.errors[0].message).to.match(/introspection is not allowed/);
+      });
+    });
+
+		it('allows other valid queries through', function() {
+      var req = request(app)
+        .post('/graphql')
+        .send({
+          query: '{ hello }'
+        });
+
+      return req.then( function(result) {
+				return expect(result.body).to.deep.equal({ data: { hello: 'world' } });
+      });
+    });
+	
   });
 });
 
